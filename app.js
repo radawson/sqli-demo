938f58c1903c033a9ba6fb75577409589405cf89
Rick Dawson <dawsora@gmail.com>
2023-07-31 21:35:28 -0400

    basic running login

---
+++ app.js
@@ -0,0 +1,43 @@
+const http = require('http'),
+    path = require('path'),
+    port = process.env.PORT || 3000,
+    express = require('express'),
+    bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const db = new sqlite3.Database(':memory:');
+db.serialize(function () {
+    db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
+    db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    db.get("SELECT * FROM user WHERE username = ? AND password = ?", [username, password], (err, row) => {
+        if (err) {
+            res.status(500).json({ "error": err.message });
+            return;
+        } else if (!row) {
+            res.redirect("/index.html#unauthorized");
+        } else {
+            res.send('Hello <b>' + row.title + '!</b><br /> This file contains all your secret data: <br><br>SECRETS <br> <br> MORE SECRETS <br><br><a href="/index.html"> Go back to login</a > ');
+            }
+    });
+
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
