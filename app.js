ea761a4f67c4db181364c95f1d58751643bdd2ea
Rick Dawson <dawsora@gmail.com>
2023-07-31 21:58:58 -0400

    clean up login box

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const db = new sqlite3.Database(':memory:');
 db.serialize(function () {
     db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
     db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
+    db.run("INSERT INTO user VALUES ('regularGuy', 'password', 'Normal User')");
 });
 
 
